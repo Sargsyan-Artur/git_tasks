80d40e40d4cf531b4da8566d52607b81e2ed24bf
Artur Sargsyan <artur_sargsyan@epam.com>
2021-03-14 22:44:15 +0300

    changed syntax by requirement

--- hoemwork3.js
+++ hoemwork3.js
@@ -1,47 +1,45 @@
 function loadArray() {
+  const listOfobj = [
+    { id: 1,
+      name: 'Elvis' 
+    },
+    
+    { id: 2,
+      name: 'Geras', 
+    },
+    
+  ];
 	
-	let listOfobj = [
-		{
-		 id: 1,
-		 name: "Elvis" 
-		},
-		{
-		 id: 2,
-		 name: "Geras" 
-		},
-	]
-	
-	return new Promise((resolve, reject) => {
-		resolve(listOfobj);
-	})	
+  return new Promise((resolve, reject) => {
+    resolve(listOfobj);
+  });	
 }
 
 
 function getItem(array, id) {
-	for (let i=0; i<array.length; i++){
-		if (array[i].id === id){
-			return new Promise((resolve, reject) => {
-				resolve(array[i])
-			})
-		}
-	}
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id === id) {
+      return new Promise((resolve, reject) => {
+        resolve(array[i]);
+      });
+    }
+  }
 	
-	return new Promise((resolve, reject) => {
-		reject(new Error("There is no such item in the array!"));
-	})
+  return new Promise((resolve, reject) => {
+    reject(new Error('There is no such item in the array!'));
+  });
 }
 
 
 async function myAsyncFunc(id) {
-	let listOfobj = await loadArray();
+  const listOfobj = await loadArray();
 	
-	try {
-		let getItemWithId = await getItem(listOfobj, id);
-		console.log(getItemWithId);
-	} catch (e) {
-		console.log(e, "eRRRRRRRRRRORRR")
-	}	
-	
+  try {
+    const getItemWithId = await getItem(listOfobj, id);
+    console.log(getItemWithId);
+  } catch (e) {
+    console.log(e, 'eRRRRRRRRRRORRR');
+  }
 }
 
-myAsyncFunc(1)
+myAsyncFunc(1);
--- homework2.js
+++ homework2.js
@@ -1,21 +1,16 @@
-function addWithDelay(...a){
-	let b = 0
-	return new Promise(function(resolve, reject){
-		for(let i of a){
-			if(i < 0){
-				 reject(new Error("Error! Some parameter is a negative number!"));
-			}else{
-				b += i;
-			}
-		}
-		setTimeout(() => resolve(b), 2000)
-		
-	})
-	
-};
+function addWithDelay(...a) {
+  let b = 0;
 
-console.log(addWithDelay(1,3,3,-3))
+  return new Promise(function (resolve, reject) {
+    for(const i of a) {
+      if(i < 0) {
+        reject(new Error('Error! Some parameter is a negative number!'));
+      }else{
+        b += i;
+      }
+    }
+    setTimeout(() => resolve(b), 2000);
+  });
+}
 
-
-
-
+console.log(addWithDelay(1,3,3,-3));
