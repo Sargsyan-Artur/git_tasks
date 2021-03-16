487322f7414f8e0aebc22b1bbc6247bf6ba2f7dd
Artur Sargsyan <artur_sargsyan@epam.com>
2021-03-16 15:18:57 +0300

    add eslint, change homework 2

--- homework2.js
+++ homework2.js
@@ -1,16 +1,19 @@
 function addWithDelay(...a) {
-  let b = 0;
+  let sum = 0;
 
   return new Promise(function (resolve, reject) {
     for(const i of a) {
       if(i < 0) {
         reject(new Error('Error! Some parameter is a negative number!'));
       }else{
-        b += i;
+        sum += i;
       }
     }
-    setTimeout(() => resolve(b), 2000);
+    setTimeout(() => resolve(sum), 2000);
   });
 }
 
-console.log(addWithDelay(1,3,3,-3));
+addWithDelay(1,3,3,3)
+  .then((resolve) => console.log(resolve))
+  .catch(err => console.log(err));
+   
